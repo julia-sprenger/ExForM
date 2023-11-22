c3596916708a752ed3484435a31eed9d4c80adf2
SPRENGER Julia, NEA/SCI/DB <julia.sprenger@oecd-nea.org>
2023-11-22 12:10:35 +0100

    Fix diction mapping for template configuration

--- .github/utils/configure_issue_template.py
+++ .github/utils/configure_issue_template.py
@@ -1,26 +1,27 @@
 import logging
 import pathlib
-from jinja2 import Environment, FileSystemLoader, UndefinedError
+from jinja2 import Environment, FileSystemLoader, UndefinedError, StrictUndefined
 
 current_folder = pathlib.Path(__file__).parent.resolve()
 issue_template_folder = current_folder / "issue_template"
+diction_folder = current_folder.parents[1] / "dictions"
 
 
-def configure_template(jinja_template_path: str | pathlib.Path) -> str:
+def configure_template(jinja_template_path: pathlib.Path, diction_folder: pathlib.Path) -> str:
     environment = Environment(
-        loader=FileSystemLoader(jinja_template_path.parent))
+        loader=FileSystemLoader(jinja_template_path.parent), undefined=StrictUndefined)
     jinja_template = environment.get_template(jinja_template_path.name)
 
-    # load dictons
-    dictons = {}
-    for d in pathlib.Path(".").glob("dicton_*.md"):
-        dicton_name = d.replace("dicton_", "").replace(".md", "")
-        dictons[dicton_name] = d.read_text()
+    # load dictions
+    dictions = {}
+    for d in diction_folder.glob("diction_*.md"):
+        diction_name = d.stem.replace("diction_", "").upper()
+        dictions[diction_name] = d.read_text()
 
     try:
-        rendered_template = jinja_template.render(dictons)
+        rendered_template = jinja_template.render(dictions)
     except UndefinedError as e:
-        raise ValueError(f'Missing dicton: {e}')
+        raise ValueError(f'Missing diction: {e}')
 
     logging.info(f"Configured template {jinja_template_path}")
 
@@ -30,7 +31,7 @@ def configure_template(jinja_template_path: str | pathlib.Path) -> str:
 def configure_all_issue_templates(issue_template_folder: str | pathlib.Path) -> None:
 
     for template_file in pathlib.Path(issue_template_folder).glob("*.yml"):
-        template = configure_template(template_file)
+        template = configure_template(template_file, diction_folder)
         save_path = current_folder / ".." / "ISSUE_TEMPLATE" / template_file.name
         pathlib.Path(save_path).write_text(template)
 
